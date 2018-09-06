132a07432647fcf091570458a0d436829220ccba
Tatianna Robinson <tatirobinson@gmail.com>
2018-09-05 20:12:44 -0400

    add app.use statement to have express look in public directory for styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const PORT = process.env.PORT || 5000;
 var express = require("express");
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (req, res){
 	res.render("index.ejs");
